0eba848c33d534c44c06442488bfcd1f93803381
shanyou <shanyou81@163.com>
2018-03-16 14:59:07 +0800

    update build script for resty

--- resty/build_resty.sh
+++ resty/build_resty.sh
@@ -11,17 +11,18 @@ mkdir -p ${WORKDIR}
 
 echo -- begin get openresty from official web site with version ${VERSION}
 pushd ${WORKDIR}
-if [ ! -d ngx_openresty-${VERSION} ]; then
+if [ ! -d openresty-${VERSION} ]; then
     wget http://openresty.org/download/openresty-${VERSION}.tar.gz
     tar xzvf openresty-${VERSION}.tar.gz
+    yum install -y readline-devel pcre-devel openssl-devel
 fi
 
 pushd openresty-${VERSION}/
-yum install -y readline-devel pcre-devel openssl-devel
 ./configure --prefix=${PREFIX_DIR} \
 --with-pcre-jit \
 --with-cc-opt="-I/usr/local/include" \
 --with-ld-opt="-L/usr/local/lib" \
---with-http_stub_status_module
+--with-http_stub_status_module \
+--with-http_v2_module
 make && make install
 popd
