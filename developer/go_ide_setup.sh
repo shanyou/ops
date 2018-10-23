d17f596f48dfef1cb7c5787d0808f8b7bd1aea4f
shanyou <shanyou@gmail.com>
2018-10-24 00:48:28 +0800

    fixed echo error

--- developer/go_ide_setup.sh
+++ developer/go_ide_setup.sh
@@ -3,43 +3,21 @@
 WORK_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 command -v go || ([[ "$GOPATH" == "" ]] && echo "need go env setup !!!" &&  exit 1)
 # get github golang tools mirrors
-echo --> begin get tools
 git clone https://github.com/golang/tools.git $GOPATH/src/golang.org/x/tools
 # get lint
 git clone https://github.com/golang/lint $GOPATH/src/golang.org/x/lint
 # install go tools
 go install golang.org/x/lint/golint
-echo --> golint installed
-
 go install golang.org/x/tools/cmd/guru
-echo --> guru installed
-
 go install golang.org/x/tools/cmd/gorename
-echo --> gorename installed
-
 go install golang.org/x/tools/cmd/goimports
-echo --> goimports installed
-
 go get -u github.com/nsf/gocode
-echo --> gocode installed
-
 go get -u github.com/alecthomas/gometalinter
-echo --> gometalinter installed
-
 go get -u github.com/rogpeppe/godef
-echo --> godef installed
-
 go get -u github.com/kisielk/errcheck
-echo --> errcheck installed
-
 go get -u github.com/jstemmer/gotags
-echo --> gotags installed
-
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
-echo --> asmfmt installed
-
 # install go vim ide
-echo --> begin install go vim ide
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/farazdagi/vim-go-ide.git ~/.vim_go_runtime
@@ -61,4 +39,4 @@ if [ "${_reg_match}" == "" ]; then
 fi
 
 source ~/.bashrc
-echo --> enjoin vim go ide use vimgo
+echo enjoin vim go ide use vimgo
